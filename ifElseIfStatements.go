621465952f59ad84e78b2e6eb74118b9c3923f31
jaechik <jaexus@hotmail.com>
2022-03-30 00:51:30 +0800

    how to check digit

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	//Insert your code here
 	//Hint if a:= ?? ; condition {  }
+	if a := 17; a%17 == 0 {
+		fmt.Println("Even")
+	} else {
+		fmt.Println("Odd")
+	}
+	a := strconv.Itoa(17)
+	fmt.Println(a)
+
+	for a > 1 {
+		fmt.Println("It has more than 1 digit")
+	}
+
 }
