f647132ed0a42563f5d652a8c6920e63175feb44
jaechik <jaexus@hotmail.com>
2022-03-30 22:43:01 +0800

    ifelseifstatement

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -5,19 +5,27 @@ import (
 	"strconv"
 )
 
+var userInput string
+
 func main() {
+	fmt.Println("Please input a number")
+	fmt.Scanln(&userInput)
+	num, _ := strconv.ParseInt(userInput, 10, 0)
 	//Insert your code here
 	//Hint if a:= ?? ; condition {  }
-	if a := 17; a%17 == 0 {
+	if num%2 == 0 {
 		fmt.Println("Even")
+		if num > 9 {
+			fmt.Println("It has more than 1 digit")
+		} else {
+			fmt.Println("It has more than 1 digit")
+		}
 	} else {
 		fmt.Println("Odd")
+		if num > 9 {
+			fmt.Println("It has more than 1 digit")
+		} else {
+			fmt.Println("It has more than 1 digit")
+		}
 	}
-	a := strconv.Itoa(17)
-	fmt.Println(a)
-
-	for a > 1 {
-		fmt.Println("It has more than 1 digit")
-	}
-
 }
